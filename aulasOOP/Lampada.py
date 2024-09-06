f27e789623afea31b076d520c4123061924f55f8
Henrique Rodrigues <tumbrl666@gmail.com>
2024-09-06 17:05:35 -0300

    arquivo teste

---
+++ aulasOOP/Lampada.py
@@ -0,0 +1,5 @@
+class Lampada:
+    def __init__(self, modelo: str, tensao: str, potencia: int ):
+        self.modelo = modelo
+        self.tensao = tensao
+        self.potencia = potencia
---
+++ aulasOOP/aula20240904.py
@@ -0,0 +1,10 @@
+class Carro:
+    def __init__(self, nome: str, marca: str, modelo: str, ano: int, numPassageiros: int ):
+        self.nome: str = nome
+        self.marca: str = marca
+        self.modelo: str = modelo
+        self.ano: int = ano
+        self.numPassageiros: int = numPassageiros
+
+
+        
