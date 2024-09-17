c00094d4887649010589faf8b6188bcbda0389ff
Henrique Rodrigues <tumbrl666@gmail.com>
2024-09-17 20:24:22 -0300

    adicionei alguns metodos para verificar senha

---
+++ listaUsuarios/usuario.py
@@ -0,0 +1,46 @@
+from datetime import date
+
+
+class Usuario:
+    def __init__(self,nomeUsuario,senha,email,dataNasc):
+        self.nomeUsuario = nomeUsuario
+        self.senha = senha
+        self.email = email
+        self.dataNasc = dataNasc
+        
+    
+    def info(self):
+        print(f"Nome de usuario : {self.nomeUsuario}")
+        print(f"email : {self.email}")
+        print(f"data de nascimento : {self.dataNasc}")
+        print(f"Senha atual : {self.senha}")
+
+    def validarSenha(self):
+            tentativaSenha = input("Informe a senha atual : ")
+            
+            if (tentativaSenha != self.senha):
+                return False
+            
+            return True   
+
+    def alterarSenha(self):
+        if self.validarSenha():            
+            novaSenha = input("Informe qual será a nova senha : ")
+            novaSenhaVerificacao = input("Digite a mesma senha novamente : ")
+            
+            if (novaSenha != novaSenhaVerificacao):
+                return False
+            
+            elif (novaSenha == novaSenhaVerificacao):
+                self.senha = novaSenha                          
+                                
+                             
+                    
+                                                      
+if __name__ == "__main__":
+    
+    user1 = Usuario("f4ther","123","henrique@gmail.com",date(2001,4,9))
+    user1.info()
+    
+    user1.alterarSenha()
+    user1.info()
